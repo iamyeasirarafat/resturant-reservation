b4fa39d527e9adcaf0d16e6ea66c645bc9e5ddfc
iamyeasirarafat <yas.yeasir@gmail.com>
2022-05-10 05:03:35 +0600

    project setup and navbar done

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#19d3ae",
+          secondary: "#0fcfec",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
