1dcbfc2a2de8852a27d26fa02f0f6a52ab20751d
iamyeasirarafat <yas.yeasir@gmail.com>
2022-05-10 06:09:23 +0600

    banner done

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import banner from "../../../assets/images/chair.png";
+import clockIcon from "../../../assets/icons/clock.svg"
+import markerIcon from "../../../assets/icons/marker.svg"
+import phoneIcon from "../../../assets/icons/phone.svg"
 import './Banner.css'
 const Banner = () => {
     return (
@@ -12,6 +15,29 @@ const Banner = () => {
                     <h2 className="text-5xl text-accent font-semibold w-10/12 mb-4">Your New Smile Starts Here</h2>
                     <p className="text-accent text-base mb-11">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the</p>
                     <button className="btn custom-btn">get started</button>
+                </div>
+            </div>
+            <div className="w-11/12  mx-auto md:grid lg:grid-cols-3 gap-x-6  md:grid-cols-2">
+                <div className="flex shadow-2xl mb-10 rounded-2xl py-10 items-center bg-gradient-to-r from-primary to-secondary">
+                    <img className="mx-6 w-20 " src={clockIcon} alt="" />
+                    <div className="">
+                        <h5 className="text-xl text-white ">Opening Hours</h5>
+                        <p className="text-base text-white mr-4">Lorem Ipsum is simply dummy text of the pri</p>
+                    </div>
+                </div>
+                <div className="flex shadow-2xl mb-10 rounded-2xl py-10 items-center bg-accent">
+                    <img className="mx-6 w-20 " src={markerIcon} alt="" />
+                    <div className="">
+                        <h5 className="text-xl text-white ">Visit our location</h5>
+                        <p className="text-base text-white mr-4">Brooklyn, NY 10036, United States</p>
+                    </div>
+                </div>
+                <div className="flex shadow-2xl mb-10 rounded-2xl py-10 items-center bg-gradient-to-r from-primary to-secondary">
+                    <img className="mx-6 w-20 " src={phoneIcon} alt="" />
+                    <div className="">
+                        <h5 className="text-xl text-white ">Contact us now</h5>
+                        <p className="text-base text-white mr-4">+000 123 456789</p>
+                    </div>
                 </div>
             </div>
         </div>
