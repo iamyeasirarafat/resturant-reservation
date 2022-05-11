4a940a7a080ef3c2ef76e62eabbe389188b6a53c
iamyeasirarafat <yas.yeasir@gmail.com>
2022-05-11 14:32:17 +0600

    service section done

---
+++ src/Components/Home/Service/Service.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import cavity from '../../../assets/images/cavity.png'
+import fluoride from '../../../assets/images/fluoride.png'
+import whitening from '../../../assets/images/whitening.png'
+const Service = () => {
+    return (
+        <div>
+            <h5 className="text-primary text-center font-semibold text-xl font">OUR SERVICE</h5>
+            <h2 className="text-center text-accent text-4xl mt-2 mb-16">Service We Provide</h2>
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-x-8 mb-24">
+                <div className="text-center rounded-lg shadow-xl">
+                    <img className=" mt-11 mx-auto w-24" src={fluoride} alt="" />
+                    <h3 className="text-accent text-xl mb-2 mt-9">Fluoride Treatment</h3>
+                    <p className="text-black text-base mx-14 mb-9">Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the</p>
+                </div>
+                <div className="text-center rounded-lg shadow-xl">
+                    <img className=" mt-11 mx-auto w-24" src={fluoride} alt="" />
+                    <h3 className="text-accent text-xl mb-2 mt-9">Fluoride Treatment</h3>
+                    <p className="text-black text-base mx-14 mb-9">Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the</p>
+                </div>
+                <div className="text-center rounded-lg shadow-xl">
+                    <img className=" mt-11 mx-auto w-24" src={fluoride} alt="" />
+                    <h3 className="text-accent text-xl mb-2 mt-9">Fluoride Treatment</h3>
+                    <p className="text-black text-base mx-14 mb-9">Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Service;
