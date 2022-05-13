2d8e98f4a82e879a8ae891ae1ef195a09acb134e
iamyeasirarafat <yas.yeasir@gmail.com>
2022-05-14 05:25:42 +0600

    login ui done

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,47 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import bg from '../../assets/images/login-bg.jpg'
 const Login = () => {
     return (
         <div>
-            
+            <div style={{
+                background: `url(${bg})`,
+                backgroundPosition: 'center'
+            }} class="hero min-h-screen">
+                <div class="hero-content flex-col lg:flex-row-reverse">
+                    <div class="text-center lg:text-left">
+                        <h1 class="text-5xl font-bold">Login now!</h1>
+                        <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    </div>
+                    <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <div class="card-body">
+                            <h2 class="text-2xl font-semibold text-center">Login</h2>
+                            <div class="form-control">
+                                <label class="label">
+                                    <span class="label-text">Email</span>
+                                </label>
+                                <input type="text" placeholder="email" class="input input-bordered" />
+                            </div>
+                            <div class="form-control">
+                                <label class="label">
+                                    <span class="label-text">Password</span>
+                                </label>
+                                <input type="text" placeholder="password" class="input input-bordered" />
+                                <label class="label">
+                                    <a href="#" class="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div class="form-control mt-6">
+                                <button class="btn">Login</button>
+                                <p class="text-sm mt-2">New in Doctors Portal? <Link class="text-secondary"to="register">Register a new one</Link></p>
+                            </div>
+                            <div class="divider">OR</div>
+                        <button class="btn btn-outline">Continue with Google</button>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
